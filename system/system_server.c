8cdedcd792e1009cdd5e7c8360e1188ea8b4af38
sandeul2 <sandeul98s@gmail.com>
2023-11-19 16:56:05 +0000

    lab9 : 멀티 스레드 동기화 & c와 c++ 연동

--- system/system_server.c
+++ system/system_server.c
@@ -10,16 +10,23 @@
 #include <gui.h>
 #include <input.h>
 #include <web_server.h>
+#include <camera_HAL.h>
+
+pthread_mutex_t system_loop_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t  system_loop_cond  = PTHREAD_COND_INITIALIZER;
+bool            system_loop_exit = false;    ///< true if main loop should exit
 
 static int five_timer = 0;
 
+void signal_exit(void);
+
 static void five_timer_handler() {
-    five_timer += 5;
+    five_timer += 10;
+    signal_exit();
     /* printf("five_timer is : %d\n", five_timer); */
 }
 
-void set_periodic_timer(long sec_delay, long usec_delay)
-{
+void set_periodic_timer(long sec_delay, long usec_delay){
    struct itimerval itimer_val = {
        .it_interval = { .tv_sec = sec_delay, .tv_usec = usec_delay },
        .it_value = { .tv_sec = sec_delay, .tv_usec = usec_delay }
@@ -37,8 +44,7 @@ int posix_sleep_ms(unsigned int timeout_ms) {
     return nanosleep(&sleep_time, NULL);
 }
 
-void *watchdog_thread(void* arg)
-{
+void *watchdog_thread(void* arg) {
     char *s = arg;
 
     printf("%s", s);
@@ -50,8 +56,7 @@ void *watchdog_thread(void* arg)
     return 0;
 }
 
-void *monitor_thread(void* arg)
-{
+void *monitor_thread(void* arg) {
     char *s = arg;
 
     printf("%s", s);
@@ -63,8 +68,7 @@ void *monitor_thread(void* arg)
     return 0;
 }
 
-void *disk_service_thread(void* arg)
-{
+void *disk_service_thread(void* arg) {
     char *s = arg;
 
     printf("%s", s);
@@ -76,17 +80,29 @@ void *disk_service_thread(void* arg)
     return 0;
 }
 
-void *camera_service_thread(void* arg)
-{
+void *camera_service_thread(void* arg) {
     char *s = arg;
 
     printf("%s", s);
+
+   toy_camera_open();
+   toy_camera_take_picture();
 
     while (1) {
         posix_sleep_ms(5000);
     }
 
     return 0;
+}
+
+void signal_exit(void) {
+    /* lab9 : 여기에 구현하세요..  종료 메시지를 보내도록.. */
+    pthread_mutex_lock(&system_loop_mutex);
+    system_loop_exit = true;
+    // broadcast or 시그널 사용
+    pthread_cond_broadcast(&system_loop_cond);
+    printf("Good bye\n");
+    pthread_mutex_unlock(&system_loop_mutex);
 }
 
 int system_server() {
@@ -110,8 +126,8 @@ int system_server() {
 
     /* lab6 : 5초 타이머를 만들어 봅시다. */
     signal(SIGALRM, five_timer_handler);
-    /* 5초 타이머 등록 */
-    set_periodic_timer(5, 0);
+    /* lab9 : 10초 타이머로 변경 */
+    set_periodic_timer(10, 0);
 
     /* lab7 : watchdog, monitor, disk_service, camera_service 스레드를 생성한다. */
     retcode = pthread_create(&watchdog_thread_tid, NULL, watchdog_thread, "watchdog thread\n");
@@ -122,6 +138,16 @@ int system_server() {
     assert(retcode == 0);
     retcode = pthread_create(&camera_service_thread_tid, NULL, camera_service_thread, "camera service thread\n");
     assert(retcode == 0);
+
+    printf("system init done.  waiting...");
+    // lab9 : 여기에 구현하세요... 여기서 cond wait로 대기한다. 10초 후 알람이 울리면 <== system 출력
+    // /* 1초 마다 wake-up 한다 */
+    pthread_mutex_lock(&system_loop_mutex); // pthread_cond_wait전 lock
+    while (system_loop_exit == false) {
+        pthread_cond_wait( &system_loop_exit, &system_loop_mutex); // lock 진행시 mutex 동일해야함
+        sleep(1);
+    }
+    pthread_mutex_unlock(&system_loop_mutex);
 
     while (1) {
         posix_sleep_ms(5000);
